a2c0d684cb3bc69b0a926515ebf1fcefd9102acb
Isaac Alejandro Lopez Castrejon <a.lopezc@hotmail.com>
2022-02-09 19:55:17 -0600

    Usar lógica booleana
    Creado por Isaac López

---
+++ Módulo 3 - Usar lógica booleana/prueba.py
@@ -0,0 +1,66 @@
+
+# Tip de práctica 1: Intenta ejecutarlo en un notebook.
+a = 97
+b = 55
+# test expression / expresión de prueba
+if a < b:
+    # statement to be run / instrucción a ejecutar
+    print(str(b))
+
+
+
+# Aplica el tip de práctica 1.
+a = 93
+b = 27
+if a >= b:
+    print(a)
+    
+    
+if a >= b:
+    print(a)
+    
+    
+if a >= b:
+    print(a)
+else:
+    print(b)
+    
+    
+if a >= b:
+    print("a es mayor o igual que b")
+elif a == b:
+    print("a es igual que b")
+    
+    
+if a > b:
+    print("a es mayor que b")
+elif a < b:
+    print("a es menor que b")
+else: 
+    print ("a es igual que b")
+    
+    
+a = 16
+b = 25
+c = 27
+if a > b:
+    if b > c:
+        print ("a es mayor que b y b es mayor que c")
+    else: 
+        print ("a es mayor que b y menor que c")
+elif a == b:
+    print ("a es igual que b")
+else:
+    print ("a es menor que b")
+    
+    
+a = 23
+b = 34
+if a == 34 or b == 34:
+    print(a + b)
+    
+    
+a = 23
+b = 34
+if a == 34 and b == 34:
+    print (a + b)
